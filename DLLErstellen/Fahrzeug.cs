c4035fb1d4a85f9fb2f30d169b4de008c4c9901d
samad.pour <samad.pour@PRE-SP>
2022-08-31 17:12:17 +0200

    Projektdateien hinzufügen.

---
+++ DLLErstellen/Fahrzeug.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLErstellen
+{
+    public partial class Fahrzeug : Component
+    {
+        public Fahrzeug()
+        {
+            InitializeComponent();
+        }
+
+        public Fahrzeug(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        private int geschwindigkeit;
+
+        public void Beschleunigen(int wert)
+        {
+            geschwindigkeit += wert;
+        }
+
+        public override string ToString()
+        {
+            return "Geschwindigkeit: " + geschwindigkeit;
+        }
+
+    }
+}
